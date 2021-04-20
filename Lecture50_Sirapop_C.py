891cf4b248f6c1c3cae22cdc812f46efc41f0fb2
ReQuirm17 <sirapop1707@gmail.com>
2021-04-20 12:57:26 +0700

    Create Lecture50_Sirapop_C.py

---
+++ Lecture50_Sirapop_C.py
@@ -0,0 +1,15 @@
+def additionnumber(a,b):
+    print(a+b)
+additionnumber(1,2)
+
+def subtractionnumber(a,b):
+    print(a-b)
+subtractionnumber(4,3)
+
+def multiplicationnumber(a,b):
+    print(a*b)
+multiplicationnumber(7,7)
+
+def divisionnumber(a,b):
+    print(a/b)
+divisionnumber(25,5)
